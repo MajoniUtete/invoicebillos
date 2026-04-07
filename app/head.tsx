2a26a4a328e7e4b4387f2b5c37ce994939c4db85
Majoni Utete <utetemajoni@gmail.com>
2026-04-07 09:30:33 +0300

    Force InvoiceBillos favicon

---
+++ app/head.tsx
@@ -0,0 +1,20 @@
+export default function Head() {
+  return (
+    <>
+      <link
+        rel="icon"
+        href="/invoicebillos-icon.svg?v=2"
+        type="image/svg+xml"
+      />
+      <link
+        rel="shortcut icon"
+        href="/invoicebillos-icon.svg?v=2"
+        type="image/svg+xml"
+      />
+      <link
+        rel="apple-touch-icon"
+        href="/invoicebillos-icon.svg?v=2"
+      />
+    </>
+  );
+}
